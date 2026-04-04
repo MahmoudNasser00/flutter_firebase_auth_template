e51c58cba5f83e9f8ebfa0ca65ecd8d4b625e7bc
Mahmoud <mestek2000@gmail.com>
2026-04-04 02:26:10 +0200

    Initialize Firebase and implement BLoC for authentication
    - Initialize Firebase in `main()` and ensure widget binding is initialized.
    - Set up dependency injection for `FirebaseAuthService`, `AuthRemoteDatasource`, and `AuthRepositoryImpl`.
    - Update `MyApp` to accept `AuthRepositoryImpl` as a required parameter.
    - Wrap `MaterialApp` with a `BlocProvider` to provide `AuthCubit` to the widget tree.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,43 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'core/services/firebase_auth_service.dart';
+import 'features/auth/data/datasources/auth_remote_datasource.dart';
+import 'features/auth/data/repositories/auth_repository_impl.dart';
+import 'features/auth/cubit/auth_cubit.dart';
 import 'features/auth/views/login_view.dart';
 import 'features/home/home_view.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  final service = FirebaseAuthService();
+  final datasource = AuthRemoteDatasource(service);
+  final repository = AuthRepositoryImpl(datasource);
+
+  runApp(MyApp(repository: repository));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AuthRepositoryImpl repository;
+
+  const MyApp({super.key, required this.repository});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FirebaseAuth.instance.currentUser == null
-          ? const LoginView()
-          : const HomeView(),
+    return BlocProvider(
+      create: (context) => AuthCubit(repository),
+
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+
+        home: FirebaseAuth.instance.currentUser == null
+            ? const LoginView()
+            : const HomeView(),
+      ),
     );
   }
 }
